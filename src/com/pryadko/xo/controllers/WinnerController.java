90bc21b7e59487bd45bb1036fb2f453fec84e6ad
Andrey Pryadko <andrey@pryadko.com>
2019-08-10 00:33:12 +0300

    WinnerController initial commit

---
+++ src/com/pryadko/xo/controllers/WinnerController.java
@@ -0,0 +1,13 @@
+package com.pryadko.xo.controllers;
+
+import com.pryadko.xo.model.Field;
+import com.pryadko.xo.model.Figure;
+
+public class WinnerController {
+
+    public Figure getWinner(final Field field) {
+        return null;
+    }
+
+
+}
