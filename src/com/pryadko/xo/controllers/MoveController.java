5de863604bba9d38261c6aad1695eff3f4c915d8
Andrey Pryadko <andrey@pryadko.com>
2019-08-09 14:56:44 +0300

    Initial MoveController

---
+++ src/com/pryadko/xo/controllers/MoveController.java
@@ -0,0 +1,21 @@
+package com.pryadko.xo.controllers;
+
+import com.pryadko.xo.model.Field;
+import com.pryadko.xo.model.Figure;
+import com.pryadko.xo.model.exceptions.InvalidPointException;
+import com.pryadko.xo.model.exceptions.PointAlreadyOccupiedException;
+
+import java.awt.*;
+
+public class MoveController {
+
+    public void applyFigure (final Field field,
+                            final Point point,
+                            final Figure figure) throws InvalidPointException, PointAlreadyOccupiedException {
+        if (field.getFigure(point) != null) {
+            throw new PointAlreadyOccupiedException();
+        }
+        field.setFigure(point, figure);
+
+    }
+}
